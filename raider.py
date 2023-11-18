263ff17bbfbc5cce3b1005b69c203bef72c83c0f
resendayter <149882833+resendayter@users.noreply.github.com>
2023-11-18 08:23:00 +0200

    Update raider.py

--- raider.py
+++ raider.py
@@ -2,7 +2,8 @@ import requests, threading #you need to pip install requests
 url = "webhook url here"
 send = "the message for the raid"
 
+def function():
+    requests.post(url,json={'content': send})
+
 while True:
-    def function():
-        requests.post(url,json={'content': send})
     function()
